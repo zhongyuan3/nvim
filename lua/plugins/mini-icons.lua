c7cacf1d5c22ae38acfcf62151672c7f7abbc3c1
zhongyuan3 <zhongyuanpan3@gmail.com>
2026-05-08 15:04:03 +0800

    feat: replace some icons and disable mini.ai

---
+++ lua/plugins/mini-icons.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-mini/mini.icons",
+  opts = {
+    directory = {
+      include = { glyph = "󰉗 " },
+    },
+    extension = {
+      c = { glyph = " " },
+      cpp = { glyph = " " },
+      cc = { glyph = " " },
+      h = { glyph = " " },
+      hpp = { glyph = " " },
+      S = { glyph = " " },
+      s = { glyph = " " },
+      asm = { glyph = " " },
+    },
+  },
+}
